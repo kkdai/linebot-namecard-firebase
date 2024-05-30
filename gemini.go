0bad7aba24a9dd3c81ba636d7a87c150113a58c0
Evan Lin <evan.if.lin@linecorp.com>
2024-05-30 16:18:11 +0800

    refactor: Update GeminiImage function to include safety settings for harmful content detection

--- gemini.go
+++ gemini.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
@@ -45,7 +46,7 @@ func GeminiImage(imgData []byte, prompt string) (string, error) {
 	resp, err := model.GenerateContent(ctx, data...)
 	log.Println("Finished processing image...", resp)
 	if err != nil {
-		log.Println(err)
+		log.Println("Gemini Image err:", err)
 		if resp != nil {
 			log.Println("result err log:", resp)
 		}
@@ -78,10 +79,14 @@ func printResponse(resp *genai.GenerateContentResponse) string {
 	for _, cand := range resp.Candidates {
 		for _, part := range cand.Content.Parts {
 			ret = ret + fmt.Sprintf("%v", part)
-			fmt.Println(part)
+			fmt.Println("part:", part)
+		}
+		if cand.Content.Parts == nil {
+			//If the response doesn't contain text, check if the prompt was blocked.
+			log.Println("Prompt blocked:", resp.PromptFeedback)
 		}
 		for _, sr := range cand.SafetyRatings {
-			log.Println("candidate safty:",sr)
+			log.Println("candidate safty:", sr)
 		}
 	}
 	return ret
